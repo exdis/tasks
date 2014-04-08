51f135761363ccb1b90b34cbb01e4ab32bf23a07
exdis <exdis@gmail.com>
2014-04-08 16:25:27 +0700

    (f) added html5 history.api support
    --HG--
    branch : dev

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 define(['angular', 'app'], function(angular, app) {
 	'use strict';
 
-	return app.config(['$routeProvider', function($routeProvider) {
+	return app.config(function($routeProvider,$locationProvider) {
+		$locationProvider.html5Mode(true);
 		$routeProvider.when('/', {
 			templateUrl: 'templates/index.html',
 			controller: 'ctrl'
 		});
 		$routeProvider.otherwise({redirectTo: '/'});
-	}]);
+	});
 
 });
