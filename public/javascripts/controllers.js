c5a40dabf8c9e9382d9382ed7e472bf2d3e535c6
Denis Kolesnikov <exsdis@gmail.com>
2014-05-03 14:52:54 +0700

    (f) improved jira time parser
    --HG--
    branch : dev

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -10,25 +10,27 @@ define(['angular', 'services', 'jquery', 'moment'], function (angular, services,
 		}
 		$scope.submit = function() {
 			var form = $scope.form;
-			var time = moment(form.time,['H m s']);
-			if(time.isValid()) {
-				var duration = moment.duration({
-					hours: time.hour(),
-					minutes: time.minute(),
-					seconds: time.second()
-				});
-				form.time = duration.asMilliseconds();
-				form.userid = $scope.currentUser;
-				$http.post('api/tasks', JSON.stringify(form)).success(function(data) {
-					if(data.status === 'OK') {
-						$('#new').modal('hide');
-						if($('.navbar-collapse').hasClass('in')) {
-							$('.navbar-toggle').click();
-						}
-						$route.reload();
+			var h = form.time.match(/\d+h/);
+			var m = form.time.match(/\d+m/);
+			var s = form.time.match(/\d+s/);
+			console.log(h);
+			var duration = moment.duration({
+				hours: h ? parseInt(h[0].replace('h','')) : 0,
+				minutes: m ? parseInt(m[0].replace('m','')) : 0,
+				seconds: s ? parseInt(s[0].replace('s','')) : 0
+			});
+			form.time = duration.asMilliseconds();
+			form.userid = $scope.currentUser;
+			$http.post('api/tasks', JSON.stringify(form)).success(function(data) {
+				if(data.status === 'OK') {
+					$('#new').modal('hide');
+					if($('.navbar-collapse').hasClass('in')) {
+						$('.navbar-toggle').click();
 					}
-				});
-			}
+					$route.reload();
+					$scope.form = {};
+				}
+			});
 		};
 		$scope.taskDelete = function(id) {
 			$http.delete('api/tasks/' + id);
--- src/filters.js
+++ src/filters.js
@@ -7,7 +7,6 @@ define(['angular', 'services','moment'], function (angular, services, moment) {
 		.filter('parseTime', function() {
 			return function(input) {
 				var d = moment.duration(parseInt(input));
-				console.log(moment.duration(input, 'seconds'));
 				var out = d.get('hours') + 'h ' + d.get('minutes') + 'm ' + d.get('seconds') + 's';
 				return out;
 			};
