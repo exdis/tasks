92bf5abd6d41305481adcee11ea5085b8bba5083
exdis <exsdis@gmail.com>
2014-09-22 16:41:07 +0700

    (f) openshift nodejs settings

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.OPENSHIFT_INTERNAL_PORT || 3000);
-app.set('ipaddr', process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1");
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || '8080');
+app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
