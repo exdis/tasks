bc3c614614d3ebe79eda8e8b926caf7986c59c3d
Denis Kolesnikov <exsdis@gmail.com>
2014-09-28 11:54:10 +0700

    (f) local dev settings

--- server/mongoose.js
+++ server/mongoose.js
@@ -2,25 +2,26 @@ var mongoose = require('mongoose');
 var log = require('./log')(module);
 var bcrypt = require('bcrypt-nodejs');
 
-mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL + 'time');
+var connectUrl = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/';
+mongoose.connect(connectUrl + 'time');
 var db = mongoose.connection;
 
 db.on('error', function (err) {
-    log.error('connection error:', err.message);
+  log.error('connection error:', err.message);
 });
 db.once('open', function callback () {
-    log.info("Connected to DB!");
+  log.info('Connected to DB!');
 });
 
 var Schema = mongoose.Schema;
 
 // Schemas
 var Task = new Schema({
-	title: { type: String, required: true },
-    link: { type: String, required: true },
-    userid: { type: String, required: true },
-    time: { type: String, required: true },
-    pubDate: { type: Date, default: Date.now }
+  title: { type: String, required: true },
+  link: { type: String, required: true },
+  userid: { type: String, required: true },
+  time: { type: String, required: true },
+  pubDate: { type: Date, default: Date.now }
 });
 
 var TaskModel = mongoose.model('Task', Task);
@@ -29,18 +30,18 @@ module.exports.TaskModel = TaskModel;
 
 var userSchema = mongoose.Schema({
 
-    email        : String,
-    password     : String,
-    cost : { type: Number, default: 0 }
+  email        : String,
+  password     : String,
+  cost : { type: Number, default: 0 }
 
 });
 
 userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
+  return bcrypt.compareSync(password, this.password);
 };
 
 var UserModel = mongoose.model('User', userSchema);
